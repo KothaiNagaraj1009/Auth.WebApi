e6376633dfaeb77f01a597e1c04e0d95dc49ee1e
KothaiNagaraj <87559312+KothaiNagaraj1009@users.noreply.github.com>
2021-08-30 00:35:23 -0700

    Add files via upload

---
+++ AUTH.DEMO/Controllers/NameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using AUTH.DEMO;
+using AUTH.DEMO.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.Demo.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager jWTAuthenticationManager;
+
+        public NameController(IJwtAuthenticationManager jWTAuthenticationManager)
+        {
+            this.jWTAuthenticationManager = jWTAuthenticationManager;
+        }
+
+        // GET: api/Name
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {  "New Jersey" , "New York"};
+        }
+
+        // GET: api/Name/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "New Jersey";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = jWTAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
---
+++ AUTH.DEMO/Controllers/UserCred.cs
@@ -0,0 +1,8 @@
+﻿namespace AUTH.DEMO.Controllers
+{
+    public class UserCred
+    {
+        public string Username { get;  set; }
+        public string Password { get;  set; }
+    }
+}
